0deb4a9e0dd629bffbb6ec358308e69c0a633514
Amanda Pagani Lima <2200564@SALAS.AULAS>
2023-04-24 20:30:10 -0300

    add app

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_DB'] = 'mysql5'
+app.config['MYSQL_DATABASE_DB'] = 'teste'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
-app.config['MYSQL_DATABASE_HOST'] = '172.18.0.46'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
 @app.route('/')
